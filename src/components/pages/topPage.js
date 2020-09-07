d5f8b254310be72ba9eca00fa23aab9a75536813
Yuki Roppongi <az0.mint9@gmail.com>
2020-09-07 22:26:12 +0900

    FunctionComponent化

--- src/components/pages/topPage.js
+++ src/components/pages/topPage.js
@@ -1,12 +1,8 @@
 import React from "react";
 import Header from "../atoms/header";
 
-class TopPage extends React.Component {
-    render() {
-        return (
-            <Header value="SampleFront"/>
-        );
-    }
+export default function TopPage() {
+    return (
+        <Header value="SampleFront"/>
+    );
 }
-
-export default TopPage;
