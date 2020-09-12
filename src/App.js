ac7891831495fb19af1c4fe2a7d97690425bbf18
Yuki Roppongi <az0.mint9@gmail.com>
2020-09-12 21:50:16 +0900

    コンポーネント構造を修正

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import TopPage from "./components/pages/top/topPage";
 import {MuiThemeProvider} from '@material-ui/core/styles'
 import {theme} from "./infrastructure/material_ui/theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Header from "./components/header";
+import TaskCardGrid from "./components/task/taskCardGrid";
 
 function App() {
   return (
       <MuiThemeProvider theme={theme}>
           <CssBaseline />
-          <TopPage/>
+          <Header value="SampleFront"/>
+          <TaskCardGrid />
       </MuiThemeProvider>
   );
 }
